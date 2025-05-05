3cdebc49c38c0864bc1797a24bb021c74173a41e
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-05-04 19:31:43 -0500

    gets user input now

--- mathloom/src/mathloom/app.py
+++ mathloom/src/mathloom/app.py
@@ -2,6 +2,7 @@
 An app to turn natural language into mathematical animations.
 """
 
+from typing import Self
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
@@ -9,19 +10,32 @@ from toga.style.pack import COLUMN, ROW
 from mainim.mvp import generate_video
 
 class Mathloom(toga.App):
-    def startup(self) -> None:
-        """Construct and show the Toga application.
+    def startup(self: Self) -> None:
+        main_box: toga.Box = toga.Box(style=Pack(direction=COLUMN, padding=10))
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        animation_input_box: toga.Box = toga.Box(style=Pack(direction=COLUMN, padding_bottom=10))
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
+        animation_label: toga.Label = toga.Label(
+            text="Describe your math animation in words: ",
+            style=Pack(padding_bottom=5)
+        )
+        self.animation_input: toga.MultilineTextInput = toga.MultilineTextInput(
+            placeholder="Write your content here",
+            style=Pack(
+                flex=1, # Allows the widget to vertically grow
+            )
+        )
+        animation_submit_button: toga.Button = toga.Button("Submit", on_press=self.create_video)
+
+        animation_input_box.add(animation_label, self.animation_input, animation_submit_button)
+        main_box.add(animation_input_box)
+
+        self.main_window: toga.MainWindow = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
+    def create_video(self: Self, _) -> None:
+        print(self.animation_input.value)
 
 def main() -> Mathloom:
     return Mathloom()
